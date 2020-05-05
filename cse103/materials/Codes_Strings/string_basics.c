809bd2bf235f1559ccb053b127084a9ec562fad4
Yeasir Rayhan <yeasirrayhan.prince@gmail.com>
2020-05-05 13:01:22 +0600

    updated_strings

--- cse103/materials/Codes_Strings/string_basics.c
+++ cse103/materials/Codes_Strings/string_basics.c
@@ -1,3 +1,31 @@
 #include <stdio.h>
-/*A String Literal, also known as a string constant or constant string, is a string of characters enclosed in double quotes,
-such as "Stay Home, Stay Safe."*/
+/*
+The C language does not have a specific "String" data type, the way some other languages such as C++ and Java do.
+Instead C stores strings of characters as arrays of chars, terminated by a null character
+*/
+int main(){
+    // Initializing strings
+    char s1[ ] = { 'h', 'e', 'l', 'l', 'o', '\0' };
+    char s2[ ] = "hello"; // Array of size six
+    char s3[ 20 ] = "hello"; // Fifteen null bytes
+    char s4[ 5 ] = "hello"; // Five chars, sacrifices the null byte
+    char s5[ 3 ] = "hello"; // Illegal
+    char s5[ 3 ] = "hello"; // Illegal
+
+    // Writing Strings Using printf and puts
+    printf("%s\n", s1);
+    printf("%s\n", s2);
+
+    puts(s1);
+    puts(s2);
+
+    // Reading Strings Using scanf and gets
+    char s5[80];
+    scanf("%s", s5);
+    printf("%s", s5);
+    char s6[80];
+    gets(s6);
+    printf("%s", s6);
+
+    return 0;
+}
---
+++ cse103/materials/Codes_Strings/string_library.c
@@ -0,0 +1,37 @@
+/*The C language includes a standard library full of routines for working with null-byte terminated arrays of chars.
+#include < string.h > to use any of these functions.
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char s[80] = "Stay Home ";
+    char t[80] = "and ";
+    char u[80] = "Stay Safe ";
+
+    // strlen(u) - returns the length of u
+    int len = strlen(s);
+    printf("Length of your concatenated string: %d\n", len);
+    len = strlen("hello world");
+
+    // strcpy(t,s) - copies contents of s to t, overwrites t
+    strcpy(t,s);
+    printf("Your copied string: %s\n", t);
+    strcpy(u, "hello");
+
+    // strcat(u,t) - concatenates content of t to u, doesn't overwrite u
+    strcat(u,t);
+    printf("Your concatenated string: %s\n", u);
+    strcat(u, " world");
+
+    // strcmp(s,t) - compares s and t, if equal returns 0, otherwise returns the difference of first mismatch
+    int cmp = strcmp(s,s);
+    printf("Comparing %s and %s : %d\n", s, s, cmp);
+    cmp = strcmp(s,t);
+    printf("Comparing %s and %s : %d\n", s, t, cmp);
+    cmp = strcmp(s,u);
+    printf("Comparing %s and %s : %d\n", s, u, cmp);
+
+
+    return 0;
+}
