a0a020940afbe0b6baee0ecd1c4252f8ed0aab5d
Yeasir Rayhan <yeasirrayhan.prince@gmail.com>
2020-07-11 22:36:01 +0600

    added_code3_slide3

--- cse103/materials/Codes3/1_typecasting.c
+++ cse103/materials/Codes3/1_typecasting.c
@@ -2,14 +2,6 @@
 
 int main() {
 
-    float c = 3.141594335;
-    printf("Division: %f\n", c); // default precision 6 = 6 digits after decimal point
-    printf("Division: %.1f\n", c); // 1 digit after decimal point
-    printf("Division: %.2f\n", c); // 2 digits after decimal point
-    printf("Division: %.3f\n", c); // 3 digits after decimal point
-    printf("Division: %.4f\n", c); // 4 digits after decimal point
-
-
 	int a, b;
 	float div; // division can contain fraction, so float
 	printf("Enter two integers: ");
